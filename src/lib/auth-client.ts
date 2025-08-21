f97520c7b098c79b822935ecbaea5a5382bac5e8
Adrian Messias <adrianmessias62@gmail.com>
2025-08-21 15:08:48 -0300

    chore: add better auth tables

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,33 +1,3 @@
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { createAuthClient } from "better-auth/react"; // make sure to import from better-auth/react
 
-import { db } from "@/db";
-import * as schema from "@/db/schema";
-
-export const auth = betterAuth({
-  emailAndPassword: {
-    enabled: true,
-  },
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    },
-  },
-  database: drizzleAdapter(db, {
-    provider: "pg",
-    schema,
-  }),
-  user: {
-    modelName: "userTable",
-  },
-  session: {
-    modelName: "sessionTable",
-  },
-  account: {
-    modelName: "accountTable",
-  },
-  verification: {
-    modelName: "verificationTable",
-  },
-});
+export const authClient = createAuthClient({});
